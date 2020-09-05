3b7d83db4d9f67f60703fabdc748cb20f69c3a22
kemmojr <you@example.com>
2020-09-05 16:29:57 +1000

    Algorithms constructor is now working and puts all of the processes of the file into an Arraylist. began implementing the FCFS algorithm

--- Algorithms.java
+++ Algorithms.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Algorithms {
@@ -7,19 +9,51 @@ public class Algorithms {
     ArrayList<SchedulerProcess> processes;
     public Algorithms(Scanner inputReader){
         //Go through the input file and convert all the process data into process classes
-        String current = inputReader.next();
+        String current = "";
         numOfProcesses = 0;
+        processes = new ArrayList<>();
         while (!current.equalsIgnoreCase("EOF")){
+            current = inputReader.next();
             if (current.equalsIgnoreCase("ID:")){
                 SchedulerProcess p = new SchedulerProcess();
-                current = inputReader.next();
-                current = inputReader.next();
-                p.setArrive(inputReader.nextInt());
+                p.setID(inputReader.next());
+                while (!current.equalsIgnoreCase("END")){
+                    if (current.equalsIgnoreCase("Arrive:")){
+                        p.setArrive(inputReader.nextInt());
+                    } else if (current.equalsIgnoreCase("ExecSize:")){
+                        p.setExecSize(inputReader.nextInt());
+                    } else if (current.equalsIgnoreCase("Priority:")){
+                        p.setPriority(inputReader.nextInt());
+                    }
+                    current = inputReader.next();
+                }
                 numOfProcesses++;
-                System.out.println("ID found");
+                processes.add(p);
             }
-            current = inputReader.next();
-            System.out.println(current);
         }
+    }
+
+    public void FCFS(){
+        //Execute the first come first served algorithm
+        ArrayList<SchedulerProcess> temp = processes;
+        Queue<SchedulerProcess> readyQueue = new PriorityQueue<>();
+        SchedulerProcess processing;//Current item that is being processed
+        int numToRemove =0;
+        int time = 0;
+        while (temp.size()>0){
+            for (int i = 0; i < temp.size(); i++) {//Iterate through all the elements of temp and and add all of the elements that have a start time that matches current time to the ready queue
+                if (temp.get(i).getArrive()==time){
+                    readyQueue.add(temp.get(i));
+                    numToRemove++;
+                }
+            }
+            for (int i = 0; i < numToRemove; i++) {
+                temp.remove(i);
+            }
+            numToRemove = 0;
+            processing = readyQueue.poll();
+
+        }
+
     }
 }
