8c3232d284a0f40fd36f90295e36ce0b68cd9f42
Tim Kemmis <kemmojr@gmail.com>
2020-09-13 13:55:04 +1000

    Changed the main file to be A1

--- A1.java
+++ A1.java
@@ -3,7 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Scheduler {
+public class A1 {
     public static void main(String args[]){
 
         Scanner reader = null;
